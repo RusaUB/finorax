0c9f8a1e3ae48d6983ce2feee9ab7e47b66ed869
RusaUB <rustamubajdulloev4@gmail.com>
2025-09-08 18:21:03 +0500

    ingest events to supabase eg

---
+++ examples/ingest_coindesk_supabase.py
@@ -0,0 +1,27 @@
+import os
+from supabase import Client, create_client
+from src.infrastructure.fetchers.clients.coindesk import CoinDeskClient
+from src.infrastructure.repositories.supabase.events import SupabaseEventRepository
+from src.application.use_cases.ingest_events import IngestEvents
+from datetime import datetime, timedelta, timezone
+
+def make_supabase() -> Client:
+    url = os.environ["SUPABASE_DEV_URL"]
+    key = os.environ["SUPABASE_DEV_KEY"]  
+    return create_client(url, key)
+
+def main():
+    sb = make_supabase()
+    coindesk_api_key = os.environ["COINDESK_API_KEY"]
+
+    feed = CoinDeskClient(api_key=coindesk_api_key)
+    events_repo = SupabaseEventRepository(sb_client=sb)
+
+    uc = IngestEvents(feed=feed, events=events_repo)
+    res = uc.run()
+    #yesterday
+    #res = uc.run(until=datetime.now(tz=timezone.utc)-timedelta(1,0,0,0,0,0,0))
+    return res
+
+if __name__ == "__main__":
+    main()
